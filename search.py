33ac6a7b3abd362dd62fb150797f4fedf3c53ba6
KrishnanandRai <knraikaip@gmail.com>
2017-08-24 13:19:47 +0530

    removed error 'exception.msg not found'

--- search.py
+++ search.py
@@ -9,15 +9,15 @@ def run(runfile):
 try:
     run(sys.argv[1])
 except Exception as e:
-    error_message = e.msg
+    #error_message = e.msg
 
     # get error type
     error_class = str(e.__class__).split()[1].strip(" <>'")
     print(error_class)
-    print(error_message)
+    print(str(e))
 
     # query to search
-    query = 'stackoverflow python' + error_class +" " + error_message 
+    query = 'stackoverflow python' + error_class +" " + str(e) 
 
     # search
     for j in search(query, tld="co.in", num=10, stop=1, pause=1):
